8034800eaa920f68e37586f099d9f445fc16c8e0
陳泳妤(KwinChen) <kwin.chen@1177tech.com.tw>
2020-05-13 11:41:31 +0800

    abc branch

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <p>hihi I am abc branch</p>
+    <p>I want to abc branch</p>
   </div>
 </template>
 
